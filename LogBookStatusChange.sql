f979ea53b474b3dd1b74e4f5e5ed74428f77e23e
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-10-27 21:38:19 +0200

    log book status change

---
+++ LogBookStatusChange.sql
@@ -0,0 +1,33 @@
+CREATE TABLE AuditLog (
+	LogID INT IDENTITY PRIMARY KEY,
+	BookID INT, 
+	StatusChange VARCHAR(10),
+	ChangeDate DATE,
+	CHECK (StatusChange IN ('available', 'borrowed')),
+)
+GO
+
+CREATE PROCEDURE sp_UpdateBookStatus @BookID INT, @NewStatus VARCHAR(10), @ChangeDate DATE 
+AS BEGIN
+	INSERT INTO AuditLog VALUES (@BookID, @NewStatus, @ChangeDate)
+
+	UPDATE Books
+		SET CurrentStatus = @NewStatus
+		WHERE BookID = @BookID;
+END
+GO
+
+CREATE TRIGGER trg_AfterLoanInsert
+ON Loans
+AFTER INSERT, UPDATE 
+AS
+BEGIN
+	DECLARE @BookID INT, 
+	@NewStatus VARCHAR(10), 
+	@now DATE = (SELECT CURRENT_TIMESTAMP);
+    
+    SELECT @BookID = BookID FROM inserted;
+	SELECT @NewStatus = CurrentStatus FROM Books WHERE BookID = @BookID
+	--SELECT * FROM inserted
+	EXEC sp_UpdateBookStatus @BookID, @NewStatus, @now
+END
